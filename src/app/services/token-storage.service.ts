657f17e1504d7f4d03b3b24a83f0afcd5157fffc
Walter Liendo <walter.liendo@gmail.com>
2022-08-05 15:55:50 -0300

    servicios

---
+++ src/app/services/token-storage.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+  roles: Array<string> = [];  
+
+  constructor(private router: Router) { }
+
+  signOut(): void {
+    window.sessionStorage.clear();
+    this.router.navigate(['/']);
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string | null {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    const user= window.sessionStorage.getItem(USER_KEY);
+    if (user){ 
+    return JSON.parse(user);
+  }
+  return {};
+}
+  public getUserName(): string {
+    if (!this.isLogged()) {
+      return null;
+    }
+    const token = this.getToken();
+    const payload = token.split('.')[1];
+    const payloadDecoded = atob(payload);
+    const values = JSON.parse(payloadDecoded);
+    const username = values.sub;
+    return username;
+  }
+
+  public isAdmin(): boolean {
+    if (!this.isLogged()) {
+      return false;
+    }
+    const token = this.getToken();
+    const payload = token.split('.')[1];
+    const payloadDecoded = atob(payload);
+    const values = JSON.parse(payloadDecoded);
+    const roles = values.roles;
+    if (roles.indexOf('ROLE_ADMIN') < 0) {
+      return false;
+    }
+    return true;
+  }
+
+  public isLogged(): boolean {
+    if (this.getToken()) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiServeUrl='http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(`${this.apiServeUrl}/api/test/all`, { responseType: 'text' });
+  }
+  getUserBoard(): Observable<any> {
+    return this.http.get(`${this.apiServeUrl}/api/test/user`, { responseType: 'text' });
+  }
+  getAdminBoard(): Observable<any> {
+    return this.http.get(`${this.apiServeUrl}/api/test/admin`, { responseType: 'text' });
+  }
+
+  }
